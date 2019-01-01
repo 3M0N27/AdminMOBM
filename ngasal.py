db569b87bc0a620ffbace215da67634378b1776d
3M0N27 <46285777+3M0N27@users.noreply.github.com>
2019-01-01 23:53:33 +0700

    Add files via upload

---
+++ ngasal.py
@@ -0,0 +1,26 @@
+# Warna
+G = "\033[32m";
+O = "\033[33m";
+B = "\033[36m";
+R = "\033[31m";
+W = "\033[0m";
+P = "\033[35m";
+
+
+name = raw_input ("name: ")
+password = raw_input ("password: ")
+if name == "Mr.3M0N27" and password == "Saya jelek":
+     print "WELCOME IN THE TOOLS MR. 3M0N27"
+     print R+"DATA ADMIN DI GRUP MOBM(Man Of Black Mask) by Mr.3M0N27"
+     print G+"    $$"
+     print G+" \\  $$  //"
+     print B+"  \\ $$ //"
+     print B+"   \\$$//"
+     print R+"    \\//"
+     print R+"    \/"
+     print "1. Mr. 3M0N27"
+     print "2. Switywebeer"
+     print "3. uti"
+     print "4. Mr. DS" 
+else:
+  print "USERNAME and PASSWORD salah silahkan pergi"
